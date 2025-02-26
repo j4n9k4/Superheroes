646ea913c6180d8549e3ac2618cd619085b96b5a
j4n9k4 <janukananayakkara@gmail.com>
2025-02-26 19:14:48 +0530

    Added the top bar Updated the SuperheroesList to take padding from scaffold

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,6 +18,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -102,9 +104,10 @@ fun HeroImage(
 }
 @Composable
 fun SuperheroesList(
+    padding: PaddingValues
 )
 {
-    LazyColumn {
+    LazyColumn(contentPadding = padding) {
         items(HeroesRepository.heroes){
             SuperheroListItem(
                 it,
@@ -121,6 +124,10 @@ fun SuperheroesList(
 fun SuperheroesAppPreview()
 {
     SuperheroesTheme {
-        SuperheroesList()
+        Scaffold() {padding ->
+            SuperheroesList(padding)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.superheroes.ui.theme.SuperheroesTheme
 
@@ -19,9 +22,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperheroesTheme {
-
+                SuperheroApp()
             }
         }
     }
 }
+@Composable
+fun SuperheroApp()
+{
+    Scaffold(
+        topBar = { TopAppBar() },
+        modifier = Modifier.fillMaxSize()
 
+    ) { padding ->
+        SuperheroesList(padding)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar()
+{
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+fun SuperheroAppPreview()
+{
+    SuperheroesTheme {
+        SuperheroApp()
+    }
+}
